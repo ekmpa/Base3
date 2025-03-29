6784210be47881ff5b94b6d097fca94720c3274e
ekmpa <emmakndp@gmail.com>
2025-03-28 21:34:23 -0400

    updated experiments, achieving highest ever rn

--- experiments.sh
+++ experiments.sh
@@ -5,24 +5,24 @@ PYTHON=python3
 # Fixed args
 VAL_RATIO="0.15"
 TEST_RATIO="0.2"
-N_RUNS="3"
-# MEM_MODE="unlim_mem"
+N_RUNS="5"
+MEM_MODE="time_window"
 W_MODE="fixed"
 
 # DGB datasets (https://zenodo.org/records/7213796#.Y1cO6y8r30o)
-DATA_ARGS=("wiki" "uci" "mooc" "USLegis" "SocialEvo" "UNvote") # "wiki" "uci" "mooc" 
+DATA_ARGS=("wiki" "uci" "USLegis" "SocialEvo" "UNvote") # "wiki" "uci" "mooc" "uci"  
 
 # Negative sampling modes
 SAMPLING_MODES=("rnd" "induc_nre" "hist_nre")
 
-MEM_MODES=("unlim_mem" "time_window")
+#MEM_MODES=("unlim_mem" "time_window")
 
 for DATA in "${DATA_ARGS[@]}"
 do
     for NEG_SAMPLE in "${SAMPLING_MODES[@]}"
     do
-        for MEM_MODE in "${MEM_MODES[@]}"
-        do 
+        #for MEM_MODE in "${MEM_MODES[@]}"
+        #do 
             echo "Running on dataset: $DATA | Sampling: $NEG_SAMPLE"
             $PYTHON baseline.py \
                 --data "$DATA" \
@@ -33,6 +33,6 @@ do
                 --mem_mode "$MEM_MODE" \
                 --w_mode "$W_MODE"
             echo "-------------------------------------------------------------"
-        done
+        #done
     done
 done
